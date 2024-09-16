7d8748f3e4bf26cf47b79618a49cb2f6d366bd30
chandimalraj <chandimalrajapaksha97@gmail.com>
2024-09-16 17:56:23 +0530

    pie chart implementation

--- src/components/app/dashboard/index.js
+++ src/components/app/dashboard/index.js
@@ -7,10 +7,13 @@ import dayjs from "dayjs";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { FieldWrapper } from "../expenses/expensesForm/ExpenseForm";
-import { getAppoinments } from "../../../services/dashBoardService";
+import {
+  getAppoinments,
+  getAppoinmentsByServiceType,
+} from "../../../services/dashBoardService";
+import DoughnutChart from "./doughnutChart/DoughnutChart";
 
 export default function Dashboard() {
-
   const getOneWeekBefore = () => {
     const today = new Date(); // Current date
     const oneWeekBefore = new Date(today); // Create a copy of today's date
@@ -21,9 +24,12 @@ export default function Dashboard() {
   const [formData, setFormData] = useState({
     startDate: getOneWeekBefore(),
     endDate: new Date(),
+    startDate02: getOneWeekBefore(),
+    endDate02: new Date(),
   });
 
-  const [data,setData] = useState()
+  const [data, setData] = useState();
+  const [dataByServiceType, setDataByServiceType] = useState();
   const navigate = useNavigate();
   const toGarage = () => {
     navigate("/nav");
@@ -39,14 +45,28 @@ export default function Dashboard() {
   };
 
   const getAppoinmentsByDate = async () => {
-    console.log(formData)
+    console.log(formData);
     try {
       const response = await getAppoinments(
         formData?.startDate,
         formData?.endDate
       );
-      setData(response?.data)
-      console.log(response)
+      setData(response?.data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAppoinmentsByDateAndServiceType = async () => {
+    console.log(formData);
+    try {
+      const response = await getAppoinmentsByServiceType(
+        formData?.startDate02,
+        formData?.endDate02
+      );
+      setDataByServiceType(response?.data);
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
@@ -55,6 +75,10 @@ export default function Dashboard() {
   useEffect(() => {
     getAppoinmentsByDate();
   }, [formData?.startDate, formData?.endDate]);
+
+  useEffect(() => {
+    getAppoinmentsByDateAndServiceType();
+  }, [formData?.startDate02, formData?.endDate02]);
 
   return (
     <Box
@@ -76,7 +100,7 @@ export default function Dashboard() {
         }}
       >
         <Grid container>
-          <Grid item lg={6} sx={{ padding:'20px' }}>
+          <Grid item lg={6} sx={{ padding: "20px" }}>
             <Box sx={{ display: "flex" }}>
               <FieldWrapper>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -137,7 +161,83 @@ export default function Dashboard() {
                 </LocalizationProvider>
               </FieldWrapper>
             </Box>
-            <StackedBarChart allData={data}/>
+            <StackedBarChart allData={data} />
+          </Grid>
+          <Grid item lg={6} sx={{ padding: "20px" }}>
+            <Box sx={{ display: "flex" }}>
+              <FieldWrapper>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="Start Date"
+                    value={dayjs(formData?.startDate02)}
+                    // value={state.action === DEF_ACTIONS.VIEW || state.action === DEF_ACTIONS.EDIT ? dayjs(formData?.expenseDate) : formData?.expenseDate}
+                    // disabled={state?.action === DEF_ACTIONS.VIEW}
+                    onChange={(e) => {
+                      console.log(dayjs(e));
+                      handleChange(
+                        dayjs(e).format("YYYY-MM-DD"),
+                        "startDate02"
+                      );
+                    }}
+                    sx={{
+                      width: "100%",
+                      marginTop: "9px",
+                      ".MuiInputBase-root": {
+                        //height: '40px', // Apply height to input container
+                        // display: 'flex', // Ensure flex container to align items properly
+                        // alignItems: 'center', // Center align items vertically
+                      },
+                      ".MuiInputBase-input": {
+                        height: "inherit", // Apply height to input
+                        padding: "10px", // Adjust padding to fit the height
+                        //boxSizing: "border-box", // Ensure padding doesn't affect height
+                      },
+                    }}
+                    defaultValue={dayjs(new Date())}
+                  />
+                </LocalizationProvider>
+              </FieldWrapper>
+              <FieldWrapper>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="End Date"
+                    value={dayjs(formData?.endDate02)}
+                    // value={state.action === DEF_ACTIONS.VIEW || state.action === DEF_ACTIONS.EDIT ? dayjs(formData?.expenseDate) : formData?.expenseDate}
+                    // disabled={state?.action === DEF_ACTIONS.VIEW}
+                    onChange={(e) => {
+                      console.log(dayjs(e));
+                      handleChange(dayjs(e).format("YYYY-MM-DD"), "endDate02");
+                    }}
+                    sx={{
+                      width: "100%",
+                      marginTop: "9px",
+                      ".MuiInputBase-root": {
+                        //height: '40px', // Apply height to input container
+                        // display: 'flex', // Ensure flex container to align items properly
+                        // alignItems: 'center', // Center align items vertically
+                      },
+                      ".MuiInputBase-input": {
+                        height: "inherit", // Apply height to input
+                        padding: "10px", // Adjust padding to fit the height
+                        //boxSizing: "border-box", // Ensure padding doesn't affect height
+                      },
+                    }}
+                    defaultValue={dayjs(new Date())}
+                  />
+                </LocalizationProvider>
+              </FieldWrapper>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Box
+                sx={{
+                  width: "70%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <DoughnutChart allData={dataByServiceType} />
+              </Box>
+            </Box>
           </Grid>
         </Grid>
       </Paper>
--- src/services/dashBoardService.js
+++ src/services/dashBoardService.js
@@ -2,7 +2,17 @@ import { axiosPrivate } from "./api";
 
 export const getAppoinments = async (startDate, endDate) => {
   const response = await axiosPrivate.get(
-    "/api/bookings/getFilteredBookings?startDate=" +
+    "/api/dashboard/getFilteredBookings?startDate=" +
+      startDate +
+      "&endDate=" +
+      endDate
+  );
+  return response;
+};
+
+export const getAppoinmentsByServiceType = async (startDate, endDate) => {
+  const response = await axiosPrivate.get(
+    "/api/dashboard/getFilteredBookingsByServiceType?startDate=" +
       startDate +
       "&endDate=" +
       endDate
